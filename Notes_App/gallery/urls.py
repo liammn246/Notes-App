6f60118245be58a9bd579e4b1c85267084b4dcad
Liam Nagel <liammnagel@gmail.com>
2025-06-12 16:20:40 -0700

    Gallery finished

--- Notes_App/gallery/urls.py
+++ Notes_App/gallery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='gallery_home'),
+    path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
 ]
--- Notes_App/gallery/views.py
+++ Notes_App/gallery/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from notes.models import Note
 
 def index(request):
-    return render(request, 'gallery/gallery.html')
+    return render(request, 'gallery/gallery.html', {'notes': Note.objects.all()})
+
+def delete_note(request, note_id):
+    if request.method == 'POST':
+        note = get_object_or_404(Note, id=note_id) #can also use .get(), and catch error
+        note.delete()
+    return redirect('gallery_home')
